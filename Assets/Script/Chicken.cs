b79a8a06f119cd3429a7872342ee74efec3c7286
Satsuna <jvbade@gmail.com>
2025-11-09 18:33:53 +0800

    feat: chicken animation

--- Assets/Script/Chicken.cs
+++ Assets/Script/Chicken.cs
@@ -4,9 +4,13 @@ using UnityEngine.AI;
 public class Chicken : MonoBehaviour
 {
     NavMeshAgent agent;
+    SpriteRenderer sprite;
+    Animator animator;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -15,5 +19,33 @@ public class Chicken : MonoBehaviour
     void OnTriggerStay2D(Collider2D collision)
     {
         agent.SetDestination(collision.transform.position);
+    }
+
+    void Update()
+    {
+        Animation();
+    }
+
+    private void Animation()
+    {
+        if (agent.velocity.magnitude > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (agent.velocity.x > 0) //facing right
+        {
+            sprite.flipX = false;
+        }
+
+        else if (agent.velocity.x < 0) //facing left
+        {
+            sprite.flipX = true;
+        }
     }
 }
